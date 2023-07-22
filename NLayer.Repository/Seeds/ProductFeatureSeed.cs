ed0291fa1ec1e931fd8757a64ba706a0c1f11c69
ozgurguclu <ozgurguclu8990@gmail.com>
2023-07-22 19:06:41 +0300

    Create Seeds

---
+++ NLayer.Repository/Seeds/ProductFeatureSeed.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+
+namespace NLayer.Repository.Seeds
+{
+    public class ProductFeatureSeed : IEntityTypeConfiguration<ProductFeature>
+    {
+        public void Configure(EntityTypeBuilder<ProductFeature> builder)
+        {
+            builder.HasData(new ProductFeature
+            {
+                Id = 1,
+                ProductId = 1,
+                Color = "Kırmızı",
+                Height = 100,
+                Width = 200,
+            },
+            new ProductFeature
+            {
+                Id = 2,
+                ProductId = 2,
+                Color = "Mavi",
+                Height = 200,
+                Width = 300,
+            });
+        }
+    }
+}
