7effddcde2b0596ae13269319de301f3bdce9aa7
ozgurguclu <ozgurguclu8990@gmail.com>
2023-07-25 20:25:28 +0300

    Create ProductWithDtoController

---
+++ NLayer.API/Controllers/ProductWithDtoController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.API.Filters;
+using NLayer.Core.Dtos;
+using NLayer.Core.Models;
+using NLayer.Core.Services;
+
+namespace NLayer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductWithDtoController : CustomBaseController
+    {
+        private readonly IProductServiceWithDto _productServiceWithDto;
+
+        public ProductWithDtoController(IProductServiceWithDto productServiceWithDto)
+        {
+            _productServiceWithDto = productServiceWithDto;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetProductsWithCategory()
+        {
+            return CreateActionResult(await _productServiceWithDto.GetProductsWithCategory());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            return CreateActionResult(await _productServiceWithDto.GetAllAsync());
+        }
+
+        [ServiceFilter(typeof(NotFoundFilter<Product>))]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return CreateActionResult(await _productServiceWithDto.GetByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ProductCreateDto productDto)
+        {
+            return CreateActionResult(await _productServiceWithDto.AddAsync(productDto));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductUpdateDto productDto)
+        {
+            return CreateActionResult(await _productServiceWithDto.UpdateAsync(productDto));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return CreateActionResult(await _productServiceWithDto.RemoveAsync(id));
+        }
+    }
+}
